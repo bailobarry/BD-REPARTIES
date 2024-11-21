13fe73ea5f3113e317d5b9ddf2ee8123e6235711
Bailo Barry Mamadou <bailo.barrymamadou@toulouse.miage.fr>
2024-11-21 11:49:14 +0100

    Savegarde des modifs sur le fichier Discipline.sql

--- TP4/Discipline.sql
+++ TP4/Discipline.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE TRIGGER t_iu_Discipline
-INSTEAD OF INSERT 
+INSTEAD OF INSERT
 ON Discipline
 FOR EACH ROW
 BEGIN 
@@ -13,3 +13,38 @@ BEGIN
         INSERT INTO Discipline_1@dbl_etupre
         VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.genred, :NEW.cds);
     END IF;
+END
+
+CREATE OR REPLACE TRIGGER t_du_Discipline
+INSTEAD OF DELETE
+ON Discipline
+FOR EACH ROW
+BEGIN 
+    IF(:NEW.genred='M') THEN
+        INSERT INTO Discipline_1@dbl_etupre
+        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
+    ELSIF(:NEW.genred='W') THEN
+        INSERT INTO Discipline_1@dbl_etupre
+        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
+    ELSIF(:NEW.genred IN ('X', 'O')) THEN
+        INSERT INTO Discipline_1@dbl_etupre
+        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.genred, :NEW.cds);
+    END IF;
+END
+
+CREATE OR REPLACE TRIGGER t_up_Discipline
+INSTEAD OF UPDATE
+ON Discipline
+FOR EACH ROW
+BEGIN 
+    IF(:NEW.genred='M') THEN
+        INSERT INTO Discipline_1@dbl_etupre
+        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
+    ELSIF(:NEW.genred='W') THEN
+        INSERT INTO Discipline_1@dbl_etupre
+        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
+    ELSIF(:NEW.genred IN ('X', 'O')) THEN
+        INSERT INTO Discipline_1@dbl_etupre
+        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.genred, :NEW.cds);
+    END IF;
+END
