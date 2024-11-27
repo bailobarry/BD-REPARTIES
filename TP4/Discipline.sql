5ef4dcfcabd59ef7400ca9777f4ebc8ed02cbf10
Bailo Barry Mamadou <bailo.barrymamadou@toulouse.miage.fr>
2024-11-27 18:32:26 +0100

    Modification du fichier Dicipline.sql

--- TP4/Discipline.sql
+++ TP4/Discipline.sql
@@ -1,50 +1,44 @@
-CREATE OR REPLACE TRIGGER t_iu_Discipline
-INSTEAD OF INSERT
+CREATE OR REPLACE TRIGGER t_iud_Discipline
+INSTEAD OF INSERT OR DELETE OR UPDATE
 ON Discipline
 FOR EACH ROW
 BEGIN 
-    IF(:NEW.genred='M') THEN
-        INSERT INTO Discipline_1@dbl_etupre
-        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
-    ELSIF(:NEW.genred='W') THEN
-        INSERT INTO Discipline_1@dbl_etupre
-        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
-    ELSIF(:NEW.genred IN ('X', 'O')) THEN
-        INSERT INTO Discipline_1@dbl_etupre
-        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.genred, :NEW.cds);
+    IF (INSERTING) THEN
+        IF(:NEW.genred='M') THEN
+            INSERT INTO Discipline_1@dbl_etupre
+            VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
+        ELSIF(:NEW.genred='W') THEN
+            INSERT INTO Discipline_1@dbl_etupre
+            VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
+        ELSIF(:NEW.genred IN ('X', 'O')) THEN
+            INSERT INTO Discipline_1@dbl_etupre
+            VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.genred, :NEW.cds);
+        END IF;
     END IF;
-END
 
-CREATE OR REPLACE TRIGGER t_du_Discipline
-INSTEAD OF DELETE
-ON Discipline
-FOR EACH ROW
-BEGIN 
-    IF(:NEW.genred='M') THEN
-        INSERT INTO Discipline_1@dbl_etupre
-        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
-    ELSIF(:NEW.genred='W') THEN
-        INSERT INTO Discipline_1@dbl_etupre
-        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
-    ELSIF(:NEW.genred IN ('X', 'O')) THEN
-        INSERT INTO Discipline_1@dbl_etupre
-        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.genred, :NEW.cds);
+    IF (DELETING) THEN
+        IF(:NEW.genred='M') THEN
+            INSERT INTO Discipline_1@dbl_etupre
+            VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
+        ELSIF(:NEW.genred='W') THEN
+            INSERT INTO Discipline_1@dbl_etupre
+            VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
+        ELSIF(:NEW.genred IN ('X', 'O')) THEN
+            INSERT INTO Discipline_1@dbl_etupre
+            VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.genred, :NEW.cds);
+        END IF;
     END IF;
-END
 
-CREATE OR REPLACE TRIGGER t_up_Discipline
-INSTEAD OF UPDATE
-ON Discipline
-FOR EACH ROW
-BEGIN 
-    IF(:NEW.genred='M') THEN
-        INSERT INTO Discipline_1@dbl_etupre
-        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
-    ELSIF(:NEW.genred='W') THEN
-        INSERT INTO Discipline_1@dbl_etupre
-        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
-    ELSIF(:NEW.genred IN ('X', 'O')) THEN
-        INSERT INTO Discipline_1@dbl_etupre
-        VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.genred, :NEW.cds);
+    IF (UPDATING) THEN
+        IF(:NEW.genred='M') THEN
+            INSERT INTO Discipline_1@dbl_etupre
+            VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
+        ELSIF(:NEW.genred='W') THEN
+            INSERT INTO Discipline_1@dbl_etupre
+            VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.cds);
+        ELSIF(:NEW.genred IN ('X', 'O')) THEN
+            INSERT INTO Discipline_1@dbl_etupre
+            VALUES(:NEW.ndd, :NEW.nomd, :NEW.typed, :NEW.genred, :NEW.cds);
+        END IF;
     END IF;
 END
