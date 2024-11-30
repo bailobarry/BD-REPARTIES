a043386d52681f048bf0c7363b19c10aa89eab6e
Bailo Barry Mamadou <bailo.barrymamadou@toulouse.miage.fr>
2024-11-30 18:35:32 +0100

    Modif de genred en v_genred

--- TP4/Pratiquer.sql
+++ TP4/Pratiquer.sql
@@ -2,20 +2,22 @@ CREATE OR REPLACE TRIGGER t_iud_Pratiquer
 INSTEAD OF INSERT OR DELETE OR UPDATE
 ON Pratiquer
 FOR EACH ROW
+DECLARE
+    v_genred CHAR(1);
 BEGIN
-    SELECT genred
+    SELECT genred INTO v_genred
     FROM Discipline
     WHERE ndd = :NEW.ndd;
 
     -- Gestion des INSERTIONS
     IF (INSERTING) THEN
-        IF (genred = 'M') THEN
+        IF (v_genred = 'M') THEN
             INSERT INTO Pratiquer_1@dbl_etupre (nda, ndd)
             VALUES (:NEW.nda, :NEW.ndd);
-        ELSIF (genred = 'W') THEN
+        ELSIF (v_genred = 'W') THEN
             INSERT INTO Pratiquer_2@dbl_etusec (nda, ndd)
             VALUES (:NEW.nda, :NEW.ndd);
-        ELSIF (genred IN ('X', 'O')) THEN
+        ELSIF (v_genred IN ('X', 'O')) THEN
             INSERT INTO Pratiquer_3@dbl_etuter (nda, ndd)
             VALUES (:NEW.nda, :NEW.ndd);
         END IF;
@@ -23,13 +25,13 @@ BEGIN
 
     -- Gestion des SUPPRESSIONS
     IF (DELETING) THEN
-        IF (genred = 'M') THEN
+        IF (v_genred = 'M') THEN
             DELETE FROM Pratiquer_1@dbl_etupre
             WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
-        ELSIF (genred = 'W') THEN
+        ELSIF (v_genred = 'W') THEN
             DELETE FROM Pratiquer_2@dbl_etusec
             WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
-        ELSIF (genred IN ('X', 'O')) THEN
+        ELSIF (v_genred IN ('X', 'O')) THEN
             DELETE FROM Pratiquer_3@dbl_etuter
             WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
         END IF;
@@ -37,17 +39,17 @@ BEGIN
 
     -- Gestion des MISE A JOUR
     IF (UPDATING) THEN
-        IF (genred = 'M') THEN
+        IF (v_genred = 'M') THEN
             UPDATE Pratiquer_1@dbl_etupre
             SET nda = :NEW.nda,
                 ndd = :NEW.ndd
             WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
-        ELSIF (genred = 'W') THEN
+        ELSIF (v_genred = 'W') THEN
             UPDATE Pratiquer_2@dbl_etusec
             SET nda = :NEW.nda,
                 ndd = :NEW.ndd
             WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
-        ELSIF (genred IN ('X', 'O')) THEN
+        ELSIF (v_genred IN ('X', 'O')) THEN
             UPDATE Pratiquer_3@dbl_etuter
             SET nda = :NEW.nda,
                 ndd = :NEW.ndd
