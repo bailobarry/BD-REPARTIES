eff6d4b0aa64bcd069eb1caf2a968cb6bc27e356
Eylul Alptekin <eylul.alptekin@toulouse.miage.fr>
2024-11-30 16:50:08 +0100

    Ajout de Pratiquer avec trigger instead of iud

---
+++ TP4/Pratiquer.sql
@@ -0,0 +1,64 @@
+CREATE OR REPLACE TRIGGER t_iud_Pratiquer
+INSTEAD OF INSERT OR DELETE OR UPDATE
+ON Pratiquer
+FOR EACH ROW
+DECLARE
+    v_genre CHAR(1);    
+    v_genred CHAR(1);   
+BEGIN
+    SELECT genre
+    INTO v_genre
+    FROM Athlete
+    WHERE nda = :NEW.nda; 
+
+    SELECT genred
+    INTO v_genred
+    FROM Discipline
+    WHERE ndd = :NEW.ndd;
+
+    IF (INSERTING) THEN
+        IF v_genre = 'M' AND v_genred = 'M' THEN
+            INSERT INTO Pratiquer_1@dbl_etupre (nda, ndd)
+            VALUES (:NEW.nda, :NEW.ndd);
+        ELSIF v_genre = 'F' AND v_genred = 'W' THEN
+            INSERT INTO Pratiquer_2@dbl_etusec (nda, ndd)
+            VALUES (:NEW.nda, :NEW.ndd);
+        ELSIF v_genre IN ('M','F') AND v_genred IN ('X', 'O') THEN
+            INSERT INTO Pratiquer_3@dbl_etuter (nda, ndd)
+            VALUES (:NEW.nda, :NEW.ndd);
+        END IF;
+    END IF;
+
+    IF (DELETING) THEN
+        IF v_genre = 'M' AND v_genred = 'M' THEN
+            DELETE FROM Pratiquer_1@dbl_etupre
+            WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
+        ELSIF v_genre = 'F' AND v_genred = 'W' THEN
+            DELETE FROM Pratiquer_2@dbl_etusec
+            WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
+        ELSIF v_genre IN ('M','F') AND v_genred IN ('X', 'O') THEN
+            DELETE FROM Pratiquer_3@dbl_etuter
+            WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
+        END IF;
+    END IF;
+
+    IF (UPDATING) THEN
+        IF v_genre = 'M' AND v_genred = 'M' THEN
+            UPDATE Pratiquer_1@dbl_etupre
+            SET nda = :NEW.nda,
+                ndd = :NEW.ndd
+            WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
+        ELSIF v_genre = 'F' AND v_genred = 'W' THEN
+            UPDATE Pratiquer_2@dbl_etusec
+            SET nda = :NEW.nda,
+                ndd = :NEW.ndd
+            WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
+        ELSIF v_genre IN ('M','F') AND v_genred IN ('X', 'O') THEN
+            UPDATE Pratiquer_3@dbl_etuter
+            SET nda = :NEW.nda,
+                ndd = :NEW.ndd
+            WHERE nda = :OLD.nda AND ndd = :OLD.ndd;
+        END IF;
+    END IF;
+END;
+/
